000632644e94c5e7b7274c2f845d50264e9ea9bb
GiovanniT <giovannilmtenereli@gmail.com>
2024-12-11 18:45:21 -0300

    Correcao para a data de nascimento

--- src/main/java/com/web2/projetoweb2/rest/SolicitacaoController.java
+++ src/main/java/com/web2/projetoweb2/rest/SolicitacaoController.java
@@ -1,23 +1,33 @@
 package com.web2.projetoweb2.rest;
 
-import com.web2.projetoweb2.dto.ResponseRelatorioDTO;
-import com.web2.projetoweb2.entity.EstadoSolicitacao;
-import com.web2.projetoweb2.entity.Solicitacao;
-import com.web2.projetoweb2.entity.SolicitacaoHistorico;
-import com.web2.projetoweb2.entity.Usuario;
-import com.web2.projetoweb2.repositorys.UsuarioRepository;
-import com.web2.projetoweb2.services.EstadoSolicitacaoService;
-import com.web2.projetoweb2.services.SolicitacoesService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.web2.projetoweb2.dto.ResponseRelatorioDTO;
+import com.web2.projetoweb2.entity.EstadoSolicitacao;
+import com.web2.projetoweb2.entity.Solicitacao;
+import com.web2.projetoweb2.entity.SolicitacaoHistorico;
+import com.web2.projetoweb2.repositorys.UsuarioRepository;
+import com.web2.projetoweb2.services.EstadoSolicitacaoService;
+import com.web2.projetoweb2.services.SolicitacoesService;
 
 @CrossOrigin
 @RestController
@@ -134,16 +144,6 @@ public class SolicitacaoController {
         Solicitacao createdSolicitacao = solicitacaoService.createSolicitacao(solicitacao);
         return new ResponseEntity<>(createdSolicitacao, HttpStatus.CREATED);
     }
-
-    // @PutMapping("/update/{id}")
-    // public ResponseEntity<Solicitacao> updateSolicitacao(@PathVariable Integer
-    // id, @RequestBody Solicitacao solicitacaoDetails) {
-    // Optional<Solicitacao> updatedSolicitacao =
-    // solicitacaoService.updateSolicitacao(id, solicitacaoDetails);
-    // return updatedSolicitacao.map(value -> new ResponseEntity<>(value,
-    // HttpStatus.OK))
-    // .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    // }
 
     @PutMapping("/pagar/{id}")
     public ResponseEntity<?> confirmarPagamento(@PathVariable Integer id) {
